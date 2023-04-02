4187856d040a6bf14dead50b07461d68f215a902
colon <coloncoryn@gmail.com>
2023-04-01 22:29:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/and101_lab6/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.and101_lab6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var petList: MutableList<String>
+    private lateinit var rvPets: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvPets = findViewById(R.id.pet_list)
+        petList = mutableListOf()
+
+        getDogImageURL()
+
+    }
+
+    private fun getDogImageURL() {
+        val client = AsyncHttpClient()
+
+        client["https://dog.ceo/api/breeds/image/random/20", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Dog", "response successful $json")
+
+                val petImageArray = json.jsonObject.getJSONArray("message")
+
+                for (i in 0 until petImageArray.length()) {
+                    petList.add(petImageArray.getString(i))
+                }
+
+                val adapter = PetAdapter(petList)
+                rvPets.adapter = adapter
+                rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
+
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
+    }
+
+}
